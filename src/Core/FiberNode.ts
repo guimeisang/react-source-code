0d5b79d6bd3c62a6fcdcffa2a9e1dcfc5c307452
guimeisang <guimeisang@users.noreply.github.com>
2021-06-15 20:21:25 +0800

    add: FiberNode的定义

---
+++ src/Core/FiberNode.ts
@@ -0,0 +1,40 @@
+function FiberNode(
+  tag: WorkTag,
+  pendingProps: mixed,
+  key: null | string,
+  mode: TypeOfMode
+) {
+  // 做为静态数据结构的属性
+  this.tag = tag
+  this.key = key
+  this.elementType = null
+  this.type = null
+  this.stateNode = null
+
+  // 用于连接其他Fiber节点形成Fiber树
+  this.return = null
+  this.child = null
+  this.sibling = null
+  this.index = 0
+
+  this.ref = null
+
+  // 做为动态的工作单元的属性
+  this.pendingProps = pendingProps
+  this.memoizedProps = null
+  this.updateQueue = null
+  this.memoizedState = null
+  this.dependencies = null
+
+  this.mode = mode
+
+  this.effectTag = NoEffect
+  this.nextEffect = null
+
+  // 调度优先级相关
+  this.lanes = NoLanes
+  this.childLanes = NoLanes
+
+  // 指向该fiber在另一次更新时对应的fiber
+  this.alternate = null
+}
